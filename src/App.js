fa377a273658bba731c362a15c07d2eb7216007c
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-04-15 18:13:18 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import Header from './components/header/Header'
-import Comments from './components/comment/Comments'
-
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from "./components/header/Header";
+import Comments from "./components/comment/Comments";
+import Tasks from "./components/pages/Tasks";
+import Admin from "./components/pages/Admin";
+import Dashboard from "./components/pages/Dashboard";
+import Leave from "./components/pages/Leave";
+import LiveEO from "./components/pages/LiveEO";
 
 function App() {
   return (
-  
+    <>
+    <Router>
     <div>
-      <Header/>
-      {users.map((user) => (
-        <Comments
-          id={user.id}
-          key={user.id}
-          name={user.name}
-          email={user.email}
-          comment={user.comments}
-          
-        />
-        ))}
+      <Header />
+      <section className="container">
+      <Comments />
+      </section>
+      <Routes>
+        <Route path='/tasks' element={<Tasks/>}/>
+        <Route path='/live-eo' element={<LiveEO/>}/>
+        <Route path='/leave' element={<Leave/>}/>
+        <Route path='/admin' element={<Admin/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+      </Routes>
+
+     
     </div>
-  
+    </Router>
+    </>
   );
 }
 
