e24e826acda56728954f92ad50c510369adcb075
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-04-17 11:01:07 +0200

    Updated

--- src/components/comment/Comments.jsx
+++ src/components/comment/Comments.jsx
@@ -3,11 +3,13 @@ import "./Comment.css";
 import Spinner from "../layout/Spinner";
 import CommentData from "../data/CommentData";
 import AddComment from "../data/AddComment";
+import ReactPaginate from "react-paginate";
 
 function Comments() {
   /*  const [name, setName] = useState('name');
   const [email, setEmail] = useState('@gmail.com');
   const [para, setPara] = useState('lorem asdfghjklmnbvcxsdrftgzhuikm ysdfghjbvc'); */
+  const [pageNumber, setPageNumber] = useState(0);
 
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -15,7 +17,6 @@ function Comments() {
   useEffect(() => {
     fetchData();
   }, []);
-
 
   // Get comment
   const fetchData = async () => {
@@ -26,7 +27,7 @@ function Comments() {
     setUsers(data);
     setLoading(false);
   };
-/* 
+  /* 
 // Creating Comment
 fetch('https://jsonplaceholder.typicode.com/posts', {
   method: 'POST',
@@ -80,37 +81,41 @@ fetch('https://jsonplaceholder.typicode.com/posts', {
     .then((response) => response.json())
     .then((json) => console.log(json));
  */
+  const usersPerPage = 12;
+  const pagesVisited = pageNumber * usersPerPage;
+  const displayUsers = users
+    .slice(pagesVisited, pagesVisited + usersPerPage)
+    .map((user) => {
+      return <CommentData key={user.id} user={user} />;
+    });
 
-
-
+    const pageCount = Math.ceil(users.length / usersPerPage)
+    const changePage = ({selected})=>{
+      setPageNumber(selected)
+    }
 
   if (!loading) {
     return (
-      <div>
+      <section className="container">
         <div className="title-container">
           <h2>Comments</h2>
-          <h2>Create comment</h2>
+          <span>Create comment</span>
         </div>
-        <AddComment/>
-        <div className="comments-container">
-          {users.map((user) => (
-            <CommentData key={user.id} user={user} />
-          ))}
-          {/* <button>Delete</button>
-       <div className="comment">
-        <div className="comments">
-          <h3></h3>
-          <h3>{email}</h3>
+        <AddComment />
+        <div className="comments-container">{displayUsers}
+        <ReactPaginate 
+        previousLabel= {'Previous'}
+        nextLabel={'Next'}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={'paginationBttns'}
+        previousLinkClassName={'previousBttn'}
+        nextLinkClassName={'nextBttn'}
+        disabledClassName={'paginationDisabled'}
+        activeClassName={'paginationActive'}
+        />
         </div>
-        <p>
-         {para}
-        </p>
-        <button>Edit</button>
-        <button>Delete</button>
-        
-      </div>   */}
-        </div>
-      </div>
+      </section>
     );
   } else {
     return <Spinner />;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,53 +1,34 @@
-import './Header.css'
-import {Link} from 'react-router-dom'
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="header">
-    
-      <div className='live'>
-      <Link to='/'>LiveEO</Link>
-        </div>
-            <ul>
-              <li>
-                <Link to='/map'>
-                  Map
-                </Link>
-              </li>
-              <li>
-                <Link to='/tasks'>
-                  Tasks
-                </Link>
-              </li>
-              <li>
-                <Link to='/dashboard'>
-                  Dashboard
-                </Link>
-              </li>
-              <li>
-                <Link to='/comments'>
-                  Comments
-                </Link>
-              </li>
-              <li>
-                <Link to='/admin'>
-                  Admin
-                </Link>
-              </li>
-              <li>
-                <Link to='/l'>
-                  L
-                </Link>
-              </li>
-            </ul>
-
-          
-
+      <div className="live">
         
-
-      
-
-    
+          <Link to="/map">LiveEO</Link>
+        
+      </div>
+      <ul>
+        <li>
+          <Link to="/map">Map</Link>
+        </li>
+        <li>
+          <Link to="/tasks">Tasks</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/comments">Comments</Link>
+        </li>
+        <li>
+          <Link to="/admin">Admin</Link>
+        </li>
+        <li>
+          <Link to="/profile">L</Link>
+        </li>
+      </ul>
     </header>
   );
 }
